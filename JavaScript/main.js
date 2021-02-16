1e8037fa812180af2f487610afe67ba6643bfc1f
Leafhill <69792376+jonas-rahlin@users.noreply.github.com>
2021-02-16 14:58:20 +0100

    New Commit
    Added Javascript for:
    Displaying the Nav Menu,
    Removing Logo on scrolldown.
    
    Added styling to Nav Menu and the hero.
    
    Restructured some HTML for
    DOM-Hierarchy purposes.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,23 @@
 function hamburgerAnimation(){
     document.querySelector(".hamburger").classList.toggle("is-active");
 };
+
+function showMenu(){
+    document.querySelector("#nav-list").classList.toggle("display");
+}
+
+document.querySelector("#nav-hamburger").addEventListener("click", ()=> {
+    hamburgerAnimation();
+    showMenu();
+})
+
+window.onscroll = function() {
+    const logo = document.querySelector("#nav-logo__logo");
+    if (window.pageYOffset > 100 && !logo.classList.contains("undisplay")) {
+        logo.classList.add("undisplay");
+    }
+
+    else if (window.pageYOffset < 1) {
+        logo.classList.remove("undisplay");
+    }
+}
