1fca58f2d1ab5ee62600ea71ab01ee5582eee5d6
Leafhill <69792376+jonas-rahlin@users.noreply.github.com>
2021-02-17 16:28:06 +0100

    New Commit
    Added new Hero Image.
    Added a Currency Selector.
    Changed some styling.
    Added CSS animations for Navigation Added Javascript for Navigation Menu functions. (color changes and onscroll display functions).

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,23 +1,71 @@
+//Starts hamburger animation
 function hamburgerAnimation(){
     document.querySelector(".hamburger").classList.toggle("is-active");
-};
-
-function showMenu(){
-    document.querySelector("#nav-list").classList.toggle("display");
 }
 
-document.querySelector("#nav-hamburger").addEventListener("click", ()=> {
-    hamburgerAnimation();
-    showMenu();
-})
+//Displays navigation menu.
+function showMenu(){
+    const menu = document.querySelector("#nav-list");  
+    menu.classList.toggle("closed");
+    menu.classList.toggle("open");
+}
 
+//Click event for hamburger menu.
+document.querySelector("#nav-hamburger").addEventListener("click", ()=> {hamburgerAnimation(); showMenu();})
+
+//Removes logo on scrolldown.
 window.onscroll = function() {
     const logo = document.querySelector("#nav-logo__logo");
-    if (window.pageYOffset > 100 && !logo.classList.contains("undisplay")) {
+    const menuAnchors = document.querySelectorAll(".nav-list__a");
+    const menuItems = document.querySelectorAll(".nav-list__item");
+    if (window.pageYOffset > 120 && !logo.classList.contains("undisplay")) {
         logo.classList.add("undisplay");
     }
-
-    else if (window.pageYOffset < 1) {
+    else if (window.pageYOffset < 120) {
         logo.classList.remove("undisplay");
     }
+    else if (window.pageYOffset > 300 && !menuAnchors[0].classList.contains("black-text")) {
+        menuAnchors.forEach(element => {
+            element.classList.add("black-text");
+        });
+        menuItems.forEach(element => {
+            element.classList.add("black-text");
+        });
+    }
+    else if (window.pageYOffset < 300) {
+        menuAnchors.forEach(element => {
+            element.classList.remove("black-text");
+        });
+        menuItems.forEach(element => {
+            element.classList.remove("black-text");
+        });
+    }
 }
+
+
+// window.onscroll = function() {
+//     const logo = document.querySelector("#nav-logo__logo");
+//     const menuAnchors = document.querySelectorAll(".nav-list__a");
+//     if (window.pageYOffset > 120 && !logo.classList.contains("undisplay")) {
+//         logo.classList.add("undisplay");
+//     }
+//     else if (window.pageYOffset < 120) {
+//         logo.classList.remove("undisplay");
+//     }
+// }
+
+
+// //Changes navigation color on scrolldown
+// window.onscroll = function() {
+//     const menuAnchors = document.querySelectorAll(".nav-list__a");
+//     else if (window.pageYOffset > 300 && !menuAnchors[0].classList.contains("black-text")) {
+//         menuAnchors.forEach(element => {
+//             element.classList.add("black-text");
+//         });
+//     }
+//     else if (window.pageYOffset < 300) {
+//         menuAnchors.forEach(element => {
+//             element.classList.remove("black-text");
+//         });
+//     }
+// }
