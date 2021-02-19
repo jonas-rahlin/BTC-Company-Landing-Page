aab4a1642c8fdb030a32980b622536263d417931
Leafhill <69792376+jonas-rahlin@users.noreply.github.com>
2021-02-19 16:28:17 +0100

    New commit.
    Shrunk the body size to create a white border on the sides.
    
    Added JS and CSS for the navigation scroll color change, to include the hamburger.
    
    Tweaked the scroll event position offset.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -13,59 +13,50 @@ function showMenu(){
 //Click event for hamburger menu.
 document.querySelector("#nav-hamburger").addEventListener("click", ()=> {hamburgerAnimation(); showMenu();})
 
-//Removes logo on scrolldown.
+//Removes logo and changes Navigation color on scrolldown.
 window.onscroll = function() {
     const logo = document.querySelector("#nav-logo__logo");
     const menuAnchors = document.querySelectorAll(".nav-list__a");
     const menuItems = document.querySelectorAll(".nav-list__item");
+    const menuHamburger = document.querySelector(".hamburger-inner")
+    
+    //Logo
     if (window.pageYOffset > 80 && !logo.classList.contains("undisplay")) {
         logo.classList.add("undisplay");
     }
-    else if (window.pageYOffset < 80) {
+    else if (window.pageYOffset < 80 && logo.classList.contains("undisplay")) {
         logo.classList.remove("undisplay");
     }
-    else if (window.pageYOffset > 280 && !menuAnchors[0].classList.contains("black-text")) {
+    
+    //Navigation
+    else if (window.pageYOffset > 250 && !menuAnchors[0].classList.contains("black-text")) {
         menuAnchors.forEach(element => {
             element.classList.add("black-text");
         });
         menuItems.forEach(element => {
             element.classList.add("black-text");
         });
+        
+        //For targeting and changing colors on :before and :after elements
+        const styleElem = document.head.appendChild(document.createElement("style"));
+        const styleElem2 = document.head.appendChild(document.createElement("style"));
+        styleElem.innerHTML = ".hamburger-inner {background: black;} .hamburger-inner:before {background: black;} .hamburger-inner:after {background: black;}";
+        styleElem2.innerHTML = ".hamburger.is-active .hamburger-inner {background: black;} .hamburger.is-active .hamburger-inner:after {background: black;}";
+        styleElem.className = "styleElem";
+        styleElem2.className = "styleElem2";
+
     }
-    else if (window.pageYOffset < 280) {
+
+    else if (window.pageYOffset < 250 && menuAnchors[0].classList.contains("black-text")) {
         menuAnchors.forEach(element => {
             element.classList.remove("black-text");
         });
         menuItems.forEach(element => {
             element.classList.remove("black-text");
         });
+        document.querySelector(".styleElem").remove();
+        document.querySelector(".styleElem2").remove();
     }
 }
 
 
-// window.onscroll = function() {
-//     const logo = document.querySelector("#nav-logo__logo");
-//     const menuAnchors = document.querySelectorAll(".nav-list__a");
-//     if (window.pageYOffset > 120 && !logo.classList.contains("undisplay")) {
-//         logo.classList.add("undisplay");
-//     }
-//     else if (window.pageYOffset < 120) {
-//         logo.classList.remove("undisplay");
-//     }
-// }
-
-
-// //Changes navigation color on scrolldown
-// window.onscroll = function() {
-//     const menuAnchors = document.querySelectorAll(".nav-list__a");
-//     else if (window.pageYOffset > 300 && !menuAnchors[0].classList.contains("black-text")) {
-//         menuAnchors.forEach(element => {
-//             element.classList.add("black-text");
-//         });
-//     }
-//     else if (window.pageYOffset < 300) {
-//         menuAnchors.forEach(element => {
-//             element.classList.remove("black-text");
-//         });
-//     }
-// }
